6b0e0ee614e63d065e6aafee239254dcb06dfdc5
Alaa Batayneh <a.batayneh@gmail.com>
2018-10-09 14:53:01 +0300

    first commit

---
+++ precache-manifest.3e047239de6ca676aaef26de054a64b6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "41331fd0116c86ffbb6e",
+    "url": "static/css/main.d6cca6bc.chunk.css"
+  },
+  {
+    "revision": "68db7e3842d1c5021d170a98defe1ef9",
+    "url": "static/media/bg.68db7e38.png"
+  },
+  {
+    "revision": "df013f5f8d6b12a9f94b",
+    "url": "static/js/1.df013f5f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "41331fd0116c86ffbb6e",
+    "url": "static/js/main.41331fd0.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "df013f5f8d6b12a9f94b",
+    "url": "static/css/1.be9ede43.chunk.css"
+  },
+  {
+    "revision": "adafba0ae2c9fd2bb6ecfa272286f1b3",
+    "url": "/index.html"
+  }
+];
